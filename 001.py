c009d81ef4cbe693369d1ea4272125354b0ad394
juliia <poshtaklon@gmail.com>
2024-03-20 22:23:37 +0200

    nothing

---
+++ 001.py
@@ -0,0 +1,17 @@
+
+# key, value = a
+# print (dict(a))
+# words = "Hello Hi Bye Goodbye List Array".split() 
+# result = {}
+
+# # Пройтися по словам попарно та додати до словника
+# for i in range(0, len(words), 2):
+#     if i + 1 < len(words):  # Перевірка, щоб уникнути виходу за межі списку
+#         result[words[i]] = words[i + 1]
+
+# print(result)
+a = "Hello hi Goodby byu"
+for key, value in a.split():
+
+
+    print a
